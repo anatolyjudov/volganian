6640126ae0dad17f595af714396a55f27f4d955a
Anatoliy Yudov <anatoly.judov@gmail.com>
2025-06-05 23:05:15 +0300

    add cname to a project

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
   eleventyConfig.addPassthroughCopy("assets/css/main.css");
   eleventyConfig.addPassthroughCopy("assets/images/");
+  eleventyConfig.addPassthroughCopy("CNAME");
 	eleventyConfig.addFilter("cssmin", function (code) {
 		return new CleanCSS({}).minify(code).styles;
 	});
