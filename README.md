a50f8fa6540547cd76153dbd4d4b4aefe4543cec
anatoliy-yudov <anatoly.judov@gmail.com>
2025-10-25 14:08:32 +0300

    texts and a lot of changes

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 ## Personal website
 
 https://volganian.me/
+
+## Development
+
+1. **Install dependencies:**
+   ```sh
+   npm install
+   ```
+
+2. **Build the site:**
+   ```sh
+   npm run build
+   ```
+
+3. **Run a local server:**
+   ```sh
+   npm start
+   ```
--- eleventy.config.js
+++ eleventy.config.js
@@ -2,8 +2,9 @@ const CleanCSS = require("clean-css");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.ignores.add("README.md");
+  eleventyConfig.ignores.add("GEMINI.md");
   eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
-  eleventyConfig.addPassthroughCopy("assets/css/main.css");
+  eleventyConfig.addPassthroughCopy("assets/css/");
   eleventyConfig.addPassthroughCopy("assets/images/");
   eleventyConfig.addPassthroughCopy("CNAME");
 	eleventyConfig.addFilter("cssmin", function (code) {
