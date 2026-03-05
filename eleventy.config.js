50cc60edce16317b02d6416780ab51445155474a
anatolyjudov <anatoly.judov@gmail.com>
2026-03-05 21:47:19 +0200

    temporary state

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,11 +2,11 @@ const CleanCSS = require("clean-css");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.ignores.add("README.md");
-  eleventyConfig.ignores.add("GEMINI.md");
   eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
   eleventyConfig.addPassthroughCopy("assets/css/");
   eleventyConfig.addPassthroughCopy("assets/images/");
   eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("**/*.jpg");
 	eleventyConfig.addFilter("cssmin", function (code) {
 		return new CleanCSS({}).minify(code).styles;
 	});
